e5fe699cba4a474f89db6885dbee738d35cbb56d
Leonardo Freua <leonardo.batista.freua@gmail.com>
2025-10-14 20:51:16 -0300

    Refactoring DepositProducer.

--- api-wallet/src/main/java/com/service/assignment/wallet/queue/producer/DepositProducer.java
+++ api-wallet/src/main/java/com/service/assignment/wallet/queue/producer/DepositProducer.java
@@ -1,40 +1,22 @@
 package com.service.assignment.wallet.queue.producer;
 
+import com.service.assignment.wallet.dto.messaging.TransactionCorrelationId;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.service.assignment.wallet.dto.messaging.TransactionDepositCorrelationId;
-import com.service.assignment.wallet.exception.DepositException;
-
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotNull;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import software.amazon.awssdk.services.sqs.SqsClient;
-import software.amazon.awssdk.services.sqs.model.SendMessageRequest;
 
 @Slf4j
+@Getter(AccessLevel.PROTECTED)
 @Service
-@RequiredArgsConstructor
-public class DepositProducer {
+public class DepositProducer extends SqsProducer<TransactionCorrelationId> {
 
-    @Value("${aws.sqs.deposit-queue-url}")
-    private String depositQueueUrl;
-
-    private final SqsClient sqsClient;
-
-    public void send(@NotNull @Valid TransactionDepositCorrelationId transactionDepositCorrelationId) {
-        try {
-            var sendMessageRequest = SendMessageRequest.builder()
-                    .queueUrl(depositQueueUrl)
-                    .messageBody(new ObjectMapper().writeValueAsString(transactionDepositCorrelationId))
-                    .build();
-
-            sqsClient.sendMessage(sendMessageRequest);
-        } catch (JsonProcessingException e) {
-            throw new DepositException(e.getMessage(), e);
-        }
+    protected DepositProducer(SqsClient sqsClient) {
+        super(sqsClient);
     }
+
+    @Value("${aws.sqs.deposit-queue.url}")
+    private String queueUrl;
 }
