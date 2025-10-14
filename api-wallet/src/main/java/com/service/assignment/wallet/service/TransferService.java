431a21a97b10d25c34ebf833ca7bd87e232736a2
Leonardo Freua <leonardo.batista.freua@gmail.com>
2025-10-14 20:56:21 -0300

    Sending transfer SQS message.

--- api-wallet/src/main/java/com/service/assignment/wallet/service/TransferService.java
+++ api-wallet/src/main/java/com/service/assignment/wallet/service/TransferService.java
@@ -1,28 +1,30 @@
 package com.service.assignment.wallet.service;
 
-import org.springframework.stereotype.Service;
-
-import com.service.assignment.wallet.bean.TransferBean;
+import com.service.assignment.wallet.bean.TransactionLogBean;
 import com.service.assignment.wallet.domain.Wallet;
-import com.service.assignment.wallet.exception.DepositException;
+import com.service.assignment.wallet.dto.api.request.TransferRequest;
+import com.service.assignment.wallet.dto.messaging.TransactionCorrelationId;
+import com.service.assignment.wallet.enums.TransactionStatus;
 import com.service.assignment.wallet.exception.TransferException;
 import com.service.assignment.wallet.exception.WalletNotFoundException;
-import com.service.assignment.wallet.exception.WithdrawException;
+import com.service.assignment.wallet.queue.producer.TransferProducer;
 import com.service.assignment.wallet.repository.WalletRepository;
-
-import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
 
 /**
  * Service responsible for handling wallet transfer operations between two
  * different users.
- * 
+ *
  * <pre>
  * This service ensures the following:
- * 
+ *
  *   - The source and target users are not the same
  *   - The source wallet exists and belongs to the requesting user
  *   - The target wallet exists and belongs to a different user
@@ -30,12 +32,12 @@ import lombok.extern.slf4j.Slf4j;
  *   - A deposit is performed into the target wallet
  *   - The transfer is logged for audit and traceability purposes
  * </pre>
- * 
+ *
  * <p>
  * All operations are performed within a transactional boundary to ensure
  * atomicity.
  * </p>
- * 
+ *
  * @author Leonardo Freua
  */
 @Slf4j
@@ -45,8 +47,7 @@ public class TransferService {
 
     private final WalletRepository walletRepository;
     private final TransactionLogService transactionLogService;
-    private final WithdrawService withdrawService;
-    private final DepositService depositService;
+    private final TransferProducer transferProducer;
 
     /**
      * Executes a money transfer between two wallets owned by different users.
@@ -61,31 +62,31 @@ public class TransferService {
      * @throws WalletNotFoundException if either the source or target wallet cannot
      *                                 be found
      */
-    @Transactional(rollbackOn = { TransferException.class, WalletNotFoundException.class, WithdrawException.class,
-            DepositException.class })
-    public void execute(@NotNull @Valid TransferBean transferBean)
+    public void execute(@Positive long sourceWalletId, @NotNull @Valid TransferRequest transferRequest)
             throws TransferException, WalletNotFoundException {
-        log.info("Transfer requested with the following criteria: {}", transferBean);
-        if (transferBean.sourceUserId() == transferBean.targetUserId()) {
+        log.info("Transfer requested with the following criteria: {}", transferRequest);
+        if (transferRequest.sourceUserId() == transferRequest.targetUserId()) {
             throw new TransferException("The source and destination user cannot be the same!");
         }
 
-        Wallet sourceWallet = walletRepository.findById(transferBean.sourceWalletId())
+        Wallet sourceWallet = walletRepository.findById(sourceWalletId)
                 .orElseThrow(() -> new WalletNotFoundException("Source wallet not found!"));
 
-        if (sourceWallet.getUser().getId() != transferBean.sourceUserId()) {
+        if (sourceWallet.getCustomer().getId() != transferRequest.sourceUserId()) {
             throw new TransferException("The provided wallet does not belong to the requesting user!");
         }
 
-        Wallet targetWallet = walletRepository.findByUserId(transferBean.targetUserId())
+        Wallet targetWallet = walletRepository.findByCustomerId(transferRequest.targetUserId())
                 .orElseThrow(() -> new WalletNotFoundException("Target wallet not found!"));
 
-        // withdrawService
-        //         .execute(new WithdrawBean(sourceWallet.getId(), transferBean.sourceUserId(), transferBean.amount()));
-        // depositService
-        //         .execute(1L, new DepositRequest(targetWallet.getId(), transferBean.targetUserId(), transferBean.amount()));
+        UUID transactionCorrelatiUuid = UUID.randomUUID();
+        transactionLogService.log(
+                TransactionLogBean.buildWithdraw(sourceWallet, transferRequest.amount(), transactionCorrelatiUuid), TransactionStatus.PROCESSING);
+        transactionLogService.log(
+                TransactionLogBean.buildDeposit(targetWallet, transferRequest.amount(), transactionCorrelatiUuid), TransactionStatus.PROCESSING);
+        transactionLogService.log(
+                TransactionLogBean.buildTransfer(sourceWallet, targetWallet, transferRequest.amount(), transactionCorrelatiUuid), TransactionStatus.PROCESSING);
 
-        // transactionLogService
-        //         .log(new TransactionLogBean(sourceWallet, targetWallet, transferBean.amount(), OperationType.TRANSFER));
+        transferProducer.send(new TransactionCorrelationId(transactionCorrelatiUuid));
     }
 }
