7bded88e47800758845beef0e1b05cf59d7e51a9
Leonardo Freua <leonardo.batista.freua@gmail.com>
2025-10-22 20:54:42 -0300

    Processor: first commit

---
+++ processor/src/main/java/com/service/wallet/processor/ProcessorApplication.java
@@ -0,0 +1,16 @@
+package com.service.wallet.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@SpringBootApplication
+@ConfigurationPropertiesScan
+public class ProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcessorApplication.class, args);
+	}
+}
